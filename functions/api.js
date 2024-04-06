c6c27200c4402abcaf0abd7d4bf49666a6be5c47
latnuk <latnuk@localhost.localdomain>
2024-04-06 11:26:31 +0530

    configured static file functions

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import ServerlessHttp from 'serverless-http';
-import ejs from 'ejs'
-import Path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 // if you want your app run locally then run (node index.js)
 export const app = express(); // run the express api
@@ -13,6 +15,9 @@ app.set('view engine', 'ejs');
 
 const router = express.Router();
 
+// for rendering static files here...
+router.use('/static' , express.static(__dirname + "/public"))
+
 router.get('/' , (req,res)=>{
     res.render('index');
 })
@@ -21,8 +26,8 @@ router.get('/git', (req,res)=>{
     res.json({"repo" : "added"})
 })
 
-router.use('/public', express.static(Path.join(__dirname, 'public')))
 
 app.use("/api", router);
+
 
 export const handler = ServerlessHttp(app);
